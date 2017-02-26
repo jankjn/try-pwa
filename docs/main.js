28f02d34e0a8c153b94b91f9fdbf3625ff606b4a
zzz <jankin.z@outlook.com>
2017-02-26 14:18:52 +0800

    add to gh-page

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+if (!navigator.onLine) {
+  document.body.classList.add('no-transition')
+  document.body.classList.add('offline')
+  setTimeout(_ => {
+    document.body.classList.remove('no-transition')
+  })
+}
+
+window.ononline = window.onoffline = _ => {
+  if (navigator.onLine) {
+    document.body.classList.remove('offline')
+  } else {
+    document.body.classList.add('offline')
+  }
+}
+
+navigator.serviceWorker.register('/sw.js')
---
+++ docs/sw.js
@@ -0,0 +1,41 @@
+const CACHE_KEY = 'try-pwa-cache-v1'
+
+async function cacheAssets() {
+  const cache = await caches.open(CACHE_KEY)
+  return cache.addAll([
+    '/',
+    '/main.js',
+    '/style.css',
+    '/creatures.css',
+  ])
+}
+
+async function clearExpiredCache() {
+  const cacheKeys = await caches.keys()
+  const expiredKeys = cacheKeys.filter(key => key !== CACHE_KEY)
+  return Promise.all(expiredKeys.map(key => caches.delete(key)))
+}
+
+self.addEventListener('install', evt => {
+  console.log('[service worker install]cache assets')
+  evt.waitUntil(cacheAssets())
+})
+self.addEventListener('activate', evt => {
+  console.log('[service worker activate]clear expired cache')
+  evt.waitUntil(clearExpiredCache())
+})
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    /**
+     * network fallback to cache
+     */
+    // fetch(evt.request)
+    //   .catch(_ => caches.match(evt.request))
+    /**
+     * cache fallback to network
+     */
+    caches.match(evt.request)
+      .then(response => response || fetch(evt.request))
+  )
+})
