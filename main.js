3162c75c2e1005fb68eca2c64ab652855058843a
zzz <jankin.z@outlook.com>
2017-02-26 13:27:42 +0800

    add service worker

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-document.body.onclick = e => {
-  e.currentTarget.classList.toggle('offline')
+if (!navigator.onLine) {
+  document.body.classList.add('no-transition')
+  document.body.classList.add('offline')
+  setTimeout(_ => {
+    document.body.classList.remove('no-transition')
+  })
 }
+
+window.ononline = window.onoffline = _ => {
+  if (navigator.onLine) {
+    document.body.classList.remove('offline')
+  } else {
+    document.body.classList.add('offline')
+  }
+}
+
+navigator.serviceWorker.register('/sw.js')
